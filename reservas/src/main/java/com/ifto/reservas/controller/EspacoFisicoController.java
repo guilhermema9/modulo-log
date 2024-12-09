f57fb6101dd28b013faedb80e32a8999dd1948cf
Yago de Lima França <yagodlfa@gmail.com>
2024-12-09 13:16:44 -0300

    update EspacoFisico.java

--- reservas/src/main/java/com/ifto/reservas/controller/EspacoFisicoController.java
+++ reservas/src/main/java/com/ifto/reservas/controller/EspacoFisicoController.java
@@ -4,21 +4,21 @@ import com.ifto.reservas.model.EspacoFisico;
 import com.ifto.reservas.repository.EspacoFisicoRepository;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 @Transactional
-@Controller
-@RequestMapping("espaco")
+@RestController
+@RequestMapping("/api/espaco")
 public class EspacoFisicoController {
 
     @Autowired
     EspacoFisicoRepository espacoFisicoRepository;
 
-    @GetMapping("/form")
+    @GetMapping("/list")
     public ModelAndView form (EspacoFisico espacoFisico, ModelMap model){
         model.addAttribute("espaco", espacoFisico);
         return new ModelAndView();
