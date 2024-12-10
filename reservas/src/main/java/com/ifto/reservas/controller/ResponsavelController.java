56bc3a11d8399de890f4410ab96975608c0f179f
Yago de Lima França <yagodlfa@gmail.com>
2024-12-10 09:58:18 -0300

    Criado ReponsavelController e seu CRUD
    falta realizar as demais validações para torna-lo restfull

---
+++ reservas/src/main/java/com/ifto/reservas/controller/ResponsavelController.java
@@ -0,0 +1,53 @@
+package com.ifto.reservas.controller;
+
+import com.ifto.reservas.model.EspacoFisico;
+import com.ifto.reservas.model.Responsavel;
+import com.ifto.reservas.repository.ResponsavelRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@Transactional
+@RestController
+@RequestMapping("/api/responsavel")
+public class ResponsavelController {
+    @Autowired
+    ResponsavelRepository responsavelRepository;
+
+    @GetMapping("/list")
+    List<Responsavel> espacoFisicosList() {
+
+        return responsavelRepository.findAll();
+    }
+
+
+    @GetMapping("/{idEspaco}")
+    Optional<Responsavel> espacoById(@PathVariable Long idEspaco) {
+
+        return responsavelRepository.findById(idEspaco);
+    }
+
+
+    @PostMapping("/add")
+    Responsavel cadastrar(@RequestBody Responsavel responsavel) {
+
+        return responsavelRepository.save(responsavel);
+    }
+
+
+    @DeleteMapping("/deletar/{idEspaco}")
+    void deletar(@PathVariable Long idEspaco) {
+
+        responsavelRepository.deleteById(idEspaco);
+    }
+
+
+    @PutMapping("/editar")
+    Responsavel editar(@RequestBody Responsavel responsavel) {
+
+        return responsavelRepository.save(responsavel);
+    }
+}
