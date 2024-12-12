3a04b1f9563f39a00a3c46b1f41ba4087b333697
Yago de Lima França <yagodlfa@gmail.com>
2024-12-12 16:41:24 -0300

    [FEAT] Hateoas implementado em Responsavel

--- reservas/src/main/java/com/ifto/reservas/controller/ResponsavelController.java
+++ reservas/src/main/java/com/ifto/reservas/controller/ResponsavelController.java
@@ -1,14 +1,19 @@
 package com.ifto.reservas.controller;
 
-import com.ifto.reservas.model.EspacoFisico;
+import com.ifto.reservas.controller.errors.ReservasNotFoundExeption;
 import com.ifto.reservas.model.Responsavel;
 import com.ifto.reservas.repository.ResponsavelRepository;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.EntityModel;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Optional;
+import java.util.stream.Collectors;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @Transactional
 @RestController
@@ -17,17 +22,25 @@ public class ResponsavelController {
     @Autowired
     ResponsavelRepository responsavelRepository;
 
-    @GetMapping("/list")
-    List<Responsavel> espacoFisicosList() {
+    @GetMapping("/all")
+    CollectionModel<EntityModel<Responsavel>> all() {
 
-        return responsavelRepository.findAll();
+        List<EntityModel<Responsavel>> responsavel = responsavelRepository.findAll().stream()
+                .map(reserva -> EntityModel.of(reserva,
+                        linkTo(methodOn(ResponsavelController.class).one(reserva.getId())).withSelfRel(),
+                        linkTo(methodOn(ResponsavelController.class).all()).withRel("Responsavel")))
+                .collect(Collectors.toList());
+        return CollectionModel.of(responsavel,linkTo(methodOn(ResponsavelController.class).all()).withSelfRel());
     }
+    @GetMapping("/{idResponsavel}")
+    EntityModel<Responsavel> one(@PathVariable Long idResponsavel) {
 
-
-    @GetMapping("/{idEspaco}")
-    Optional<Responsavel> espacoById(@PathVariable Long idEspaco) {
-
-        return responsavelRepository.findById(idEspaco);
+        Responsavel reserva = responsavelRepository.findById(idResponsavel).orElseThrow(
+                () -> new ReservasNotFoundExeption(idResponsavel)
+        );
+        return  EntityModel.of(reserva,
+                linkTo(methodOn(ResponsavelController.class).one(idResponsavel)).withSelfRel(),
+                linkTo(methodOn(ResponsavelController.class).all()).withRel("Responsavel"));
     }
 
     @DeleteMapping("/deletar/{idEspaco}")
