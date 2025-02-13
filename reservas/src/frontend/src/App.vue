c156befcfc51f535aa9bfc752e2d2e8ee394ef63
Yago de Lima França <134020465+Yago-Lima@users.noreply.github.com>
2025-02-13 10:03:31 -0300

    hello world Vue + Bootsrap

---
+++ reservas/src/frontend/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <b-container>
+      <b-row>
+        <b-col>
+          <h1>Hello World with Bootstrap</h1>
+        </b-col>
+      </b-row>
+    </b-container>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
