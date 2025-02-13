94cf599a6b2aaea0fbd593e93b27c69db4c03dee
Yago de Lima França <134020465+Yago-Lima@users.noreply.github.com>
2025-02-13 10:04:18 -0300

    Update App.vue

--- reservas/src/frontend/src/App.vue
+++ reservas/src/frontend/src/App.vue
@@ -3,7 +3,7 @@
     <b-container>
       <b-row>
         <b-col>
-          <h1>Hello World with Bootstrap</h1>
+          <h1>Olá Mundo !!</h1>
         </b-col>
       </b-row>
     </b-container>
